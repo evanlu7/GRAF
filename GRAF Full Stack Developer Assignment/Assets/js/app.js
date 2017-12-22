99cbfa966c01384e3fad9b04c071071706bb41b4
Evan Lu <evanlu7@gmail.com>
2017-12-21 16:55:05 -0800

    need to fix copy functinoality

--- GRAF Full Stack Developer Assignment/Assets/js/app.js
+++ GRAF Full Stack Developer Assignment/Assets/js/app.js
@@ -1,15 +1,37 @@
-$("#shorten").click(function() {
+// Shorten URL with Google API
+$("#shorten").click(function writeToPage() {
     var longUrl = $("#shortenURL").val();
     $.ajax({
         url: 'https://www.googleapis.com/urlshortener/v1/url?&key=AIzaSyCnibvuj2xdGGnSJrHS54NJ56kKFnWTsVo',
         type: 'POST',
         contentType: 'application/json; charset=utf-8',
         data: '{longUrl:"' + longUrl + '"}',
-        success: function(response) {
+        success: function (response) {
             console.log(response["id"]);
+            var shortURL = "<a href=" + response["id"] + " target=_BLANK >" + response["id"] + "</a>";             
+             document.getElementById("form7").innerHTML = shortURL;
 
-            $("#result").html(response["id"]);
-            bootbox.alert("Shortened url: <a href=" + response["id"] + " target=_BLANK >" + response["id"] + "</a>");
+
+
+             // Change to COPY  on click
+            $("form7").css('color', 'black');
+            document.getElementById("shorten").innerHTML = '<h3 class="text-center font-weight-bold" id="copy">COPY</h3>';
+
+
+// Copy Link
+
+
+
+
+
+
+
+
         }
     });
+
+          
+
+
+
 });
